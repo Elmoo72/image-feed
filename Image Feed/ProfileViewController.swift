3bbb32e5d5746778bafc26e64b905d6d42dc39a5
Elmoo72 <makar3452@yandex.ru>
2025-07-07 00:32:46 +0500

    спринт 9 урок 4

---
+++ Image Feed/ProfileViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var ProfileImage: UIImageView!
+   
+    @IBOutlet weak var NameLable: UILabel!
+   
+    
+    @IBOutlet weak var TagLable: UILabel!
+    
+    
+    @IBOutlet weak var TextLable: UILabel!
+    
+    @IBAction func LogOut(_ sender: Any) {
+    }
+    @IBOutlet weak var Exit: UIButton!
+}
