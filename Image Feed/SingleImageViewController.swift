d3b6fdcbd049428d10e6978aea6e8d46133ccbd5
Elmoo72 <makar3452@yandex.ru>
2025-07-12 14:23:26 +0500

    Добален SingleImageView нужно исправить кнопку возврата

--- Image Feed/SingleImageViewController.swift
+++ Image Feed/SingleImageViewController.swift
@@ -1,5 +1,20 @@
 import UIKit
 final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return } // 1
+            imageView.image = image // 2
+        }
+    }
+    
+    @IBAction private func didTapBackButton() {
+            dismiss(animated: true, completion: nil)
+        }
     
     @IBOutlet weak var imageView: UIImageView!
+   
+    override func viewDidLoad() {
+          super.viewDidLoad()
+          imageView.image = image
+      }
 }
